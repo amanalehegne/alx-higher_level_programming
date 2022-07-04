9fb1e6d77599f594803434632068f0ba92917ee6
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2022-07-04 18:26:26 +0300

    0x0A

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module 2-is_same_class.py
+same instance
+and methods
+"""
+
+
+def inherits_from(obj, a_class):
+    """ is the objec is exactly an instance of the specified class """
+    return (type(obj) is not a_class and issubclass(type(obj), a_class))
